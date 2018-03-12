756d3ba3305f06c4f7dbd330bd7743fe80166771
Josh Lehman <jalehman37@gmail.com>
2018-03-12 08:58:52 -0700

    Version 1.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mapp "1.2.1-SNAPSHOT"
+(defproject mapp "1.2.1"
   :description "Starcity's Member Application"
   :url "http://apply.starcity.com"
   :license {:name "Eclipse Public License"
